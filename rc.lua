7259016fed0aa493e0363124663f7adb2f6dfa7f
ArtyIF <artyomisflash@mail.ru>
2021-08-23 08:37:04 +0300

    changed theme to gtk

--- rc.lua
+++ rc.lua
@@ -47,7 +47,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-local theme_path = string.format("%s/.config/awesome/themes/%s/theme.lua", os.getenv("HOME"), "default")
+local theme_path = string.format("%s/.config/awesome/themes/%s/theme.lua", os.getenv("HOME"), "gtk")
 beautiful.init(theme_path)
 
 -- This is used later as the default terminal and editor to run.
