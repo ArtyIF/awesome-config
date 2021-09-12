1669b3d980ef207b686896e929ca955ddd14953a
ArtyIF <artyomisflash@mail.ru>
2021-09-12 17:21:35 +0300

    added padding to the system tray

--- components/widgets/system_tray.lua
+++ components/widgets/system_tray.lua
@@ -6,12 +6,21 @@ this.icon_size = nil
 this.mode = "horizontal"
 this.reverse = false
 
+this.margin_top = 4
+this.margin_right = 4
+this.margin_bottom = 4
+this.margin_left = 4
+
 function this.create_widget()
     local system_tray = wibox.widget.systray()
+
     system_tray.base_size = this.icon_size
     system_tray.horizontal = this.mode == "horizontal"
     system_tray.reverse = this.reverse
-    return system_tray
+
+    this.widget = wibox.container.margin(system_tray, this.margin_top, this.margin_right, this.margin_bottom, this.margin_left)
+    
+    return this.widget
 end
 
 return this
