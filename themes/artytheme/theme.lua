43c432155f91a59adfd8a857d3ea1185a96624d4
ArtyIF <artyomisflash@mail.ru>
2021-09-12 14:44:26 +0300

    new palette!

--- themes/artytheme/theme.lua
+++ themes/artytheme/theme.lua
@@ -18,9 +18,9 @@ theme.font          = gtk_vars.font_family .. " " .. gtk_vars.font_size
 
 local function hex_to_srgb(color)
     color = string.gsub(color, "#", "")
-    if string.len(color) == 6 then
+    if string.len(color) >= 6 then
         return tonumber("0x" .. string.sub(color, 1, 2)) / 255, tonumber("0x" .. string.sub(color, 3, 4)) / 255, tonumber("0x" .. string.sub(color, 5, 6)) / 255
-    elseif string.len(color) == 3 then
+    else
         return tonumber("0x" .. string.sub(color, 1, 1)) / 255, tonumber("0x" .. string.sub(color, 2, 2)) / 255, tonumber("0x" .. string.sub(color, 3, 3)) / 255
     end
 end
@@ -74,44 +74,41 @@ local function darken_color(color, amount)
     return "#" .. string.format("%02x", math.floor(r * 255)) .. string.format("%02x", math.floor(g * 255)) .. string.format("%02x", math.floor(b * 255))
 end
 
--- possible values for colors
--- todo: color button imageboxes too somehow
--- todo: adjust the colors
-local possible_colors =
+-- possible palettes, taken from https://tailwindcolor.com/
+local possible_palettes =
 {
-    "#ff0000",
-    "#ff7f00",
-    "#ffff00",
-    "#7fff00",
-    "#00ff00",
-    "#00ff7f",
-    "#00ffff",
-    "#007fff",
-    -- "#0000ff", -- too low contrast
-    -- "#7f00ff", -- looks cool, too low contrast
-    "#ff00ff",
-    "#ff007f",
+    { neutral_bg = "#1C1917", neutral_fg = "#FAFAF9", color_900 = "#7F1D1D", color_800 = "#991B1B", color_700 = "#B91C1C", color_600 = "#DC2626", color_500 = "#EF4444", color_400 = "#F87171", color_300 = "#FCA5A5", color_200 = "#FECACA", color_100 = "#FEE2E2", color_50 = "#FEF2F2" }, -- red
+    { neutral_bg = "#1C1917", neutral_fg = "#FAFAF9", color_900 = "#7C2D12", color_800 = "#9A3412", color_700 = "#C2410C", color_600 = "#EA580C", color_500 = "#F97316", color_400 = "#FB923C", color_300 = "#FDBA74", color_200 = "#FED7AA", color_100 = "#FFEDD5", color_50 = "#FFF7ED" }, -- orange
+    { neutral_bg = "#1C1917", neutral_fg = "#FAFAF9", color_900 = "#78350F", color_800 = "#92400E", color_700 = "#B45309", color_600 = "#D97706", color_500 = "#F59E0B", color_400 = "#FBBF24", color_300 = "#FCD34D", color_200 = "#FDE68A", color_100 = "#FEF3C7", color_50 = "#FFFBEB" }, -- amber
+    { neutral_bg = "#1C1917", neutral_fg = "#FAFAF9", color_900 = "#713F12", color_800 = "#854D0E", color_700 = "#A16207", color_600 = "#CA8A04", color_500 = "#EAB308", color_400 = "#FACC15", color_300 = "#FDE047", color_200 = "#FEF08A", color_100 = "#FEF9C3", color_50 = "#FEFCE8" }, -- yellow
+    { neutral_bg = "#1C1917", neutral_fg = "#FAFAF9", color_900 = "#365314", color_800 = "#3F6212", color_700 = "#4D7C0F", color_600 = "#65A30D", color_500 = "#84CC16", color_400 = "#A3E635", color_300 = "#BEF264", color_200 = "#D9F99D", color_100 = "#ECFCCB", color_50 = "#F7FEE7" }, -- lime
+    { neutral_bg = "#171717", neutral_fg = "#FAFAFA", color_900 = "#14532D", color_800 = "#166534", color_700 = "#15803D", color_600 = "#16A34A", color_500 = "#22C55E", color_400 = "#4ADE80", color_300 = "#86EFAC", color_200 = "#BBF7D0", color_100 = "#DCFCE7", color_50 = "#F0FDF4" }, -- green
+    { neutral_bg = "#171717", neutral_fg = "#FAFAFA", color_900 = "#064E3B", color_800 = "#065F46", color_700 = "#047857", color_600 = "#059669", color_500 = "#10B981", color_400 = "#34D399", color_300 = "#6EE7B7", color_200 = "#A7F3D0", color_100 = "#ECFDF5", color_50 = "#ECFDF5" }, -- emerald
+    { neutral_bg = "#18181B", neutral_fg = "#FAFAFA", color_900 = "#134E4A", color_800 = "#115E59", color_700 = "#0F766E", color_600 = "#059669", color_500 = "#14B8A6", color_400 = "#2DD4BF", color_300 = "#5EEAD4", color_200 = "#99F6E4", color_100 = "#F0FDFA", color_50 = "#F0FDFA" }, -- teal
+    { neutral_bg = "#18181B", neutral_fg = "#FAFAFA", color_900 = "#164E63", color_800 = "#155E75", color_700 = "#0E7490", color_600 = "#0891B2", color_500 = "#14B8A6", color_400 = "#22D3EE", color_300 = "#67E8F9", color_200 = "#A5F3FC", color_100 = "#ECFEFF", color_50 = "#ECFEFF" }, -- cyan
+    { neutral_bg = "#18181B", neutral_fg = "#FAFAFA", color_900 = "#0C4A6E", color_800 = "#075985", color_700 = "#0369A1", color_600 = "#0284C7", color_500 = "#0EA5E9", color_400 = "#38BDF8", color_300 = "#7DD3FC", color_200 = "#BAE6FD", color_100 = "#F0F9FF", color_50 = "#F0F9FF" }, -- light blue
+    { neutral_bg = "#18181B", neutral_fg = "#FAFAFA", color_900 = "#1E3A8A", color_800 = "#1E40AF", color_700 = "#1D4ED8", color_600 = "#2563EB", color_500 = "#3B82F6", color_400 = "#60A5FA", color_300 = "#93C5FD", color_200 = "#BFDBFE", color_100 = "#DBEAFE", color_50 = "#EFF6FF" }, -- blue
+    { neutral_bg = "#18181B", neutral_fg = "#FAFAFA", color_900 = "#312E81", color_800 = "#3730A3", color_700 = "#4338CA", color_600 = "#4F46E5", color_500 = "#6366F1", color_400 = "#818CF8", color_300 = "#A5B4FC", color_200 = "#C7D2FE", color_100 = "#E0E7FF", color_50 = "#EEF2FF" }, -- indigo
+    { neutral_bg = "#171717", neutral_fg = "#FAFAFA", color_900 = "#4C1D95", color_800 = "#5B21B6", color_700 = "#6D28D9", color_600 = "#7C3AED", color_500 = "#8B5CF6", color_400 = "#A78BFA", color_300 = "#C4B5FD", color_200 = "#DDD6FE", color_100 = "#EDE9FE", color_50 = "#F5F3FF" }, -- violet
+    { neutral_bg = "#171717", neutral_fg = "#FAFAFA", color_900 = "#581C87", color_800 = "#6B21A8", color_700 = "#7E22CE", color_600 = "#9333EA", color_500 = "#A855F7", color_400 = "#C084FC", color_300 = "#D8B4FE", color_200 = "#E9D5FF", color_100 = "#F3E8FF", color_50 = "#FAF5FF" }, -- purple
+    { neutral_bg = "#1C1917", neutral_fg = "#FAFAF9", color_900 = "#701A75", color_800 = "#86198F", color_700 = "#A21CAF", color_600 = "#9333EA", color_500 = "#D946EF", color_400 = "#E879F9", color_300 = "#F0ABFC", color_200 = "#F5D0FE", color_100 = "#FAE8FF", color_50 = "#FDF4FF" }, -- fuchsia
+    { neutral_bg = "#1C1917", neutral_fg = "#FAFAF9", color_900 = "#831843", color_800 = "#9F1239", color_700 = "#BE185D", color_600 = "#DB2777", color_500 = "#EC4899", color_400 = "#F472B6", color_300 = "#F9A8D4", color_200 = "#FBCFE8", color_100 = "#FCE7F3", color_50 = "#FDF2F8" }, -- pink
+    { neutral_bg = "#1C1917", neutral_fg = "#FAFAF9", color_900 = "#881337", color_800 = "#9F1239", color_700 = "#BE123C", color_600 = "#E11D48", color_500 = "#F43F5E", color_400 = "#FB7185", color_300 = "#FDA4AF", color_200 = "#FECDD3", color_100 = "#FFE4E6", color_50 = "#FFF1F2" }, -- rose
 }
 
 math.randomseed(os.time())
-local random_color_chosen = possible_colors[math.random(#possible_colors)]
+local chosen_palette = possible_palettes[math.random(#possible_palettes)]
 
-theme.bg_normal     = darken_color(random_color_chosen, 0.05)
-theme.bg_focus      = darken_color(random_color_chosen, 0.25)
-theme.bg_urgent     = random_color_chosen
-theme.bg_minimize   = theme.bg_focus .. "7f"
+theme.bg_normal     = chosen_palette.neutral_bg
+theme.bg_focus      = chosen_palette.color_900
+theme.bg_urgent     = chosen_palette.color_600
+theme.bg_minimize   = theme.bg_focus .. "3f"
 theme.bg_systray    = theme.bg_normal
 
-theme.fg_normal     = best_fg(theme.bg_normal) .. "bf"
-theme.fg_focus      = best_fg(theme.bg_focus)
-theme.fg_urgent     = best_fg(theme.bg_urgent)
-theme.fg_minimize   = best_fg(theme.bg_normal) .. "bf"
-
-theme.useless_gap   = 0
-theme.border_width  = 0
-theme.border_normal = theme.bg_normal
-theme.border_focus  = theme.bg_focus
-theme.border_marked = theme.bg_urgent
+theme.fg_normal     = chosen_palette.neutral_fg
+theme.fg_focus      = chosen_palette.color_50
+theme.fg_urgent     = chosen_palette.color_50
+theme.fg_minimize   = chosen_palette.neutral_fg
 
 -- There are other variable sets
 -- overriding the default one when
@@ -123,14 +120,7 @@ theme.border_marked = theme.bg_urgent
 -- mouse_finder_[color|timeout|animate_timeout|radius|factor]
 -- prompt_[fg|bg|fg_cursor|bg_cursor|font]
 -- hotkeys_[bg|fg|border_width|border_color|shape|opacity|modifiers_fg|label_bg|label_fg|group_margin|font|description_font]
--- theme.titlebar_bg_normal = theme.bg_normal .. "bf"
--- theme.titlebar_bg_focus = theme.bg_focus .. "bf"
-
-local taglist_square_size = 32
-theme.taglist_squares_sel = theme_assets.taglist_squares_sel(0, "#00000000")
-theme.taglist_squares_unsel = theme_assets.taglist_squares_sel(
-    taglist_square_size, theme.bg_minimize
-)
+theme.taglist_bg_occupied = theme.bg_minimize
 
 -- Variables set for theming notifications:
 -- notification_font
@@ -178,7 +168,7 @@ theme.titlebar_maximized_button_normal_active = theme_path.."titlebar/maximized.
 theme.titlebar_maximized_button_focus_active  = theme_path.."titlebar/maximized.png"
 
 theme.wallpaper = function(s)
-    return theme_assets.wallpaper(theme.bg_normal, theme.fg_normal, theme.bg_urgent, s)
+    return theme_assets.wallpaper(theme.bg_normal, theme.fg_focus, theme.bg_urgent, s)
 end
 
 -- You can use your own layout icons like this:
@@ -208,11 +198,14 @@ theme.awesome_icon = theme_assets.awesome_icon(
 -- from /usr/share/icons and /usr/share/icons/hicolor will be used.
 theme.icon_theme = "/usr/share/icons/breeze-dark"
 
-theme = theme_assets.recolor_titlebar(theme, theme.bg_urgent, "normal", nil, "active")
-theme = theme_assets.recolor_titlebar(theme, theme.bg_urgent, "focus", nil, "active")
-theme = theme_assets.recolor_titlebar(theme, "#bfbfbf", "normal", nil, "inactive") -- this one ignores transparency unfortunately
-theme = theme_assets.recolor_titlebar(theme, theme.fg_focus, "focus", nil, "inactive")
-theme = theme_assets.recolor_titlebar(theme, theme.fg_focus, "normal", "hover", "inactive")
+theme = theme_assets.recolor_titlebar(theme, chosen_palette.neutral_fg, "normal", nil, "inactive")
+theme = theme_assets.recolor_titlebar(theme, chosen_palette.color_50, "focus", nil, "inactive")
+theme = theme_assets.recolor_titlebar(theme, chosen_palette.color_300, "focus", "hover", nil)
+theme = theme_assets.recolor_titlebar(theme, chosen_palette.color_300, "normal", "hover", nil)
+theme = theme_assets.recolor_titlebar(theme, chosen_palette.color_500, "focus", "press", nil)
+theme = theme_assets.recolor_titlebar(theme, chosen_palette.color_500, "normal", "press", nil)
+theme = theme_assets.recolor_titlebar(theme, chosen_palette.color_300, "normal", nil, "active")
+theme = theme_assets.recolor_titlebar(theme, chosen_palette.color_300, "focus", nil, "active")
 
 return theme
 
