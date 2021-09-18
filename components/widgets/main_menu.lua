6efc7e638f5804f68802de7b007735f9fc0f5dd1
ArtyIF <artyomisflash@mail.ru>
2021-09-18 13:06:23 +0300

    overhauled the menu, now using menubar

--- components/widgets/main_menu.lua
+++ components/widgets/main_menu.lua
@@ -1,3 +1,4 @@
+
 -- standard awesome stuff
 local gears = require("gears")
 local awful = require("awful")
@@ -5,73 +6,92 @@ local awful = require("awful")
 local wibox = require("wibox")
 -- theme handling library
 local beautiful = require("beautiful")
+-- menugen from menubar
+local menu_gen = require("menubar.menu_gen")
+local ass = require("menubar.utils")
 -- hotkeys popup, we might not need it
 local hotkeys_popup = require("awful.hotkeys_popup")
 require("awful.hotkeys_popup.keys")
 
-local this = {menus = {}}
-
--- generate the app menu (sync) and require it
--- todo: replace with a better option since these files aren't easily editable neither by alacarte or kmenuedit
-os.execute("xdg_menu --fullmenu --format awesome --root-menu /etc/xdg/menus/applications.menu > ~/.config/awesome/appmenu.lua")
-require("appmenu")
-
--- awesomewm menu
-this.menus.awesome = {
-    { "Hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end }, -- remove?
-    { "Local Awesome Config", "code " .. gears.filesystem.get_configuration_dir() .. "/awesome-config.code-workspace" },
-    { "Restart", awesome.restart },
- }
-
--- favorites menu
--- todo: make this as part of xdgmenu somehow
-this.menus.favorites = {
-    { "Web Browser", "firefox" },
-    { "Files", "pcmanfm" },
-    { "Terminal", "konsole" },
-    { "Discord", "discord" },
-    { "Steam", "steam-runtime" },
+local this = {
+    menus = {
+        favorites = {
+            { "Web Browser", "firefox" },
+            { "Files", "pcmanfm" },
+            { "Terminal", "konsole" },
+            { "Discord", "discord" },
+            { "Steam", "steam-runtime" },
+        },
+        apps = {},
+        awesome = {
+            { "Hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end }, -- remove?
+            { "Local Awesome Config", "code " .. gears.filesystem.get_configuration_dir() .. "/awesome-config.code-workspace" },
+            { "Restart", awesome.restart },
+        },
+        session = {
+            { "Log Out", function() awesome.quit() end }
+        }
+    }
 }
 
--- taken from roblox docs, clones a table
--- todo: move to separate file for everything
-local function shallow_copy(original)
-    local copy = {}
-    for key, value in pairs(original) do
-        copy[key] = value
-    end
-    return copy
-end
+function this.build_menu()
+    menu_gen.generate(function (entries)
+        for id, category in pairs(menu_gen.all_categories) do
+            table.insert(this.menus.apps, { id, {}, category.icon }) -- todo: fix icons
+        end
 
--- the entries that are actually going to be in the menu. first favorites, then all the xdg categories, then awesomewm and then an option to log out
-this.items = shallow_copy(xdgmenu)
-table.insert(this.items, 1, { "Favorites", this.menus.favorites })
-table.insert(this.items, { "Awesome", this.menus.awesome, beautiful.awesome_icon })
-table.insert(this.items, { "Log Out", function() awesome.quit() end }) -- todo: make it fade out again, but properly
+        for _, entry in pairs(entries) do
+            for _, category in pairs(this.menus.apps) do
+                if category[1] == entry.category then
+                    table.insert(category[2], { entry.name, entry.cmdline, entry.icon })
+                    break
+                end
+            end
+        end
 
--- the menu itself
-this.menu = awful.menu({ items = this.items })
+        for id = #this.menus.apps, 1, -1 do -- doing it in reverse prevents shifting
+            local category = this.menus.apps[id]
+            if #category[2] == 0 then
+                table.remove(this.menus.apps, id)
+            else
+                category[1] = menu_gen.all_categories[category[1]].name
+                table.sort(category[2], function (a, b) return string.byte(a[1]) < string.byte(b[1]) end)
+            end
+        end
 
-root.buttons({
-    awful.button({}, 1, function ()
-        this.menu:hide()
-    end),
-    awful.button({}, 3, function ()
-        this.menu:toggle()
+        table.sort(this.menus.apps, function (a, b) return string.byte(a[1]) < string.byte(b[1]) end)
+
+        this.items = {}
+        table.insert(this.items, { "Favorites", this.menus.favorites })
+        for _, category in pairs(this.menus.apps) do
+            table.insert(this.items, category)
+        end
+        table.insert(this.items, { "Awesome", this.menus.awesome, beautiful.awesome_icon })
+        table.insert(this.items, { "Exit", this.menus.session }) -- todo: make it fade out again, but properly
+
+        this.menu = awful.menu({ items = this.items })
+        root.buttons({
+            awful.button({}, 1, function ()
+                this.menu:hide()
+            end),
+            awful.button({}, 3, function ()
+                this.menu:toggle()
+            end)
+        })
+
+        this.button:buttons({
+            awful.button({}, 1, function ()
+                this.menu:toggle()
+            end)
+        })
     end)
-})
+end
 
 function this.create_widget()
     -- todo: make it more elaborate than a simple dropdown. i'm leaning windows 10 style
-    local menu_button = awful.widget.button({ image = "/usr/share/icons/breeze-dark/actions/32/application-menu.svg" })
-
-    menu_button:buttons({
-        awful.button({}, 1, function ()
-            this.menu:toggle()
-        end)
-    })
-
-    return menu_button
+    this.button = awful.widget.button({ image = "/usr/share/icons/breeze-dark/actions/32/application-menu.svg" })
+    this.build_menu()
+    return this.button
 end
 
 return this
