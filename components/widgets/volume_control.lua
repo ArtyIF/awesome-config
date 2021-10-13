f22ab7dfe4a5e9fecf6c4f4f2ba21eb13e73ecce
Artyom Fomin <artyomisflash@mail.ru>
2021-10-13 19:44:02 +0000

    couple adjustments for my really slow laptop

--- components/widgets/volume_control.lua
+++ components/widgets/volume_control.lua
@@ -7,11 +7,12 @@ local theme_vars = require("beautiful").get()
 
 local this = {}
 
-this.cmd = "amixer -D pulse "
-this.get_arg = "get Master"
-this.up_arg = "set Master 5%+"
-this.down_arg = "set Master 5%-"
-this.toggle_arg = "set Master toggle"
+this.cmd = "pactl "
+this.get_volume_arg = "get-sink-volume 0"
+this.get_mute_arg = "get-sink-mute 0"
+this.up_arg = "set-sink-volume 0 +5%"
+this.down_arg = "set-sink-volume 0 -5%"
+this.toggle_arg = "set-sink-mute 0 toggle"
 
 this.icons_path = "/usr/share/icons/breeze-dark/status/symbolic/"
 this.icon_high = "audio-volume-high-symbolic.svg"
@@ -37,33 +38,42 @@ function this.callback(volume, muted)
     this.text_widget.text = volume .. "%"
 end
 
-function this.parse_cmd_out(cmd)
+function this.execute_and_get_output(cmd)
     local file = io.popen(cmd)
     local out = file:read('*all')
     file:close()
-    local volume = tonumber(out:match("(%d?%d?%d)%%"))
-    local muted = out:match("%[o[nf]f?%]") == "[off]"
+    return out
+end
+
+function this.parse_cmd_out(volume_cmd, mute_cmd)
+    local volume_out = this.execute_and_get_output(volume_cmd)
+    local mute_out = this.execute_and_get_output(mute_cmd)
+    local volume = tonumber(volume_out:match("(%d?%d?%d)%%"))
+    local muted = mute_out:match("Mute: yes")
     this.callback(volume, muted)
     return volume, muted
 end
 
 function this.get()
-    return this.parse_cmd_out(this.cmd .. this.get_arg)
+    return this.parse_cmd_out(this.cmd .. this.get_volume_arg, this.cmd .. this.get_mute_arg)
 end
 
 function this.up()
-    awful.spawn.with_shell("canberra-gtk-play --id=audio-volume-change")
-    return this.parse_cmd_out(this.cmd .. this.up_arg)
+    --awful.spawn.with_shell("canberra-gtk-play --id=audio-volume-change") -- todo: make toggleable. disabled because it freezes awesomewm on my laptop for quite a while
+    this.execute_and_get_output(this.cmd .. this.up_arg)
+    return this.get()
 end
 
 function this.down()
-    awful.spawn.with_shell("canberra-gtk-play --id=audio-volume-change")
-    return this.parse_cmd_out(this.cmd .. this.down_arg)
+    --awful.spawn.with_shell("canberra-gtk-play --id=audio-volume-change")
+    this.execute_and_get_output(this.cmd .. this.down_arg)
+    return this.get()
 end
 
 function this.toggle()
-    awful.spawn.with_shell("canberra-gtk-play --id=audio-volume-change")
-    return this.parse_cmd_out(this.cmd .. this.toggle_arg)
+    --awful.spawn.with_shell("canberra-gtk-play --id=audio-volume-change")
+    this.execute_and_get_output(this.cmd .. this.toggle_arg)
+    return this.get()
 end
 
 
--- rc.lua
+++ rc.lua
@@ -331,6 +331,6 @@ else
     awful.spawn.spawn("picom --experimental-backends --config=" .. gears.filesystem.get_xdg_config_home() .. "awesome/picom/picom.conf", false)
 end
 if not os.execute("pgrep thunderbird") then
-    awful.spawn.spawn("kdocker thunderbird", false) -- make sure to install Simple Startup Minimizer (https://addons.thunderbird.net/en-US/thunderbird/addon/simple-startup-minimizer/) and Minimize On Close (https://addons.thunderbird.net/en-US/thunderbird/addon/minimize-on-close/)
+    awful.spawn.spawn("kdocker -d 60 thunderbird", false) -- make sure to install Simple Startup Minimizer (https://addons.thunderbird.net/en-US/thunderbird/addon/simple-startup-minimizer/) and Minimize On Close (https://addons.thunderbird.net/en-US/thunderbird/addon/minimize-on-close/)
 end
 awful.spawn.spawn("copyq", false)
