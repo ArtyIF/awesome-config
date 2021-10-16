6bf199d70fc14c046fa2689918dc1fa2c17f0835
ArtyIF <artyomisflash@mail.ru>
2021-10-17 00:17:37 +0300

    the bottom bar now autohides

--- components/wibars/bottom.lua
+++ components/wibars/bottom.lua
@@ -1,4 +1,5 @@
 local awful = require("awful")
+local gears = require("gears")
 local wibox = require("wibox")
 
 local this = {}
@@ -12,6 +13,27 @@ function this.create_bar(s)
         bar_height = 24
     end
     local bar = awful.wibar({ position = "bottom", screen = s, height = bar_height, ontop = true })
+    bar.y = 1080 - 1
+    bar:struts({ bottom = 0 })
+
+    local hide_timer = gears.timer({
+        timeout = 0.25,
+        autostart = false,
+        call_now = false,
+        callback = function ()
+            bar.y = s.geometry.height - 1
+            bar:struts({ bottom = 0 })
+        end
+    })
+    bar:connect_signal("mouse::enter", function ()
+        hide_timer:stop()
+        bar.y = s.geometry.height - bar_height
+        bar:struts({ bottom = 0 })
+    end)
+    bar:connect_signal("mouse::leave", function ()
+        hide_timer:start()
+    end)
+
     bar:setup {
         layout = wibox.layout.align.horizontal,
         nil,
