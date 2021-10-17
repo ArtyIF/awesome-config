746e4d4f6707e2774aa7f6fd4576192cdb8ff730
ArtyIF <artyomisflash@mail.ru>
2021-10-17 13:44:47 +0300

    some adjustments for fullscreen stuff

--- components/signals/wibars_ontop_when_not_fullscreen.lua
+++ components/signals/wibars_ontop_when_not_fullscreen.lua
@@ -2,9 +2,16 @@ local this = {}
 
 this.affected_wibars = {}
 
-function this.signal_callback(c)
+function this.signal_callback()
+    if client.focus == nil then
+        for _, wibar in ipairs(this.affected_wibars) do
+            wibar.ontop = true
+        end
+        return
+    end
+    local is_window_fullscreen = (client.focus.width >= client.focus.screen.geometry.width and client.focus.height >= client.focus.screen.geometry.height) or client.focus.type == "fullscreen"
     for _, wibar in ipairs(this.affected_wibars) do
-        wibar.ontop = not (c.fullscreen or c.type == "fullscreen")
+        wibar.ontop = not is_window_fullscreen
     end
 end
 
