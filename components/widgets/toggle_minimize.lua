dd9f2750a7bd170ef324667f48794f16438c9afe
ArtyIF <artyomisflash@mail.ru>
2021-10-17 20:41:59 +0300

    improved toggle minimize colors

--- components/widgets/toggle_minimize.lua
+++ components/widgets/toggle_minimize.lua
@@ -6,13 +6,13 @@ local wibox = require("wibox")
 local this = {minimized = false, minimize_history = {}}
 
 function this.create_widget(s)
-    local toggle_minimize = wibox.container.background(nil, beautiful.get().bg_focus)
+    local toggle_minimize = wibox.container.background(nil, beautiful.get().tasklist_bg_minimize)
     toggle_minimize.forced_width = 4
 
     toggle_minimize:buttons({
         awful.button({}, 1, function ()
             if not this.minimized then this.minimize_history = {} end
-            for i, client in ipairs(s.all_clients) do
+            for _, client in ipairs(s.all_clients) do
                 if not this.minimized then
                     this.minimize_history[client.window] = client.minimized
                     client.minimized = true
@@ -21,6 +21,11 @@ function this.create_widget(s)
                 end
             end
             this.minimized = not this.minimized
+            if this.minimized then
+                toggle_minimize.bg = beautiful.get().tasklist_bg_focus
+            else
+                toggle_minimize.bg = beautiful.get().tasklist_bg_minimize
+            end
         end)
     })
 
