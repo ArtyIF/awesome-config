914619e9b72ad7683097899bfb68bfcd1a502244
ArtyIF <artyomisflash@mail.ru>
2021-10-22 13:11:32 +0300

    work in progress adaptive colors

--- components/signals/titlebar.lua
+++ components/signals/titlebar.lua
@@ -1,9 +1,41 @@
 local awful = require("awful")
 local gears = require("gears")
 local wibox = require("wibox")
+local naughty = require("naughty")
 local theme_vars = require("beautiful").get()
 
 local this = {}
+
+local gdk = require("lgi").Gdk
+
+local function get_dominant_color(c)
+    local c_content = gears.surface(c.content)
+    local c_geometry = c:geometry()
+
+    local colors = {}
+
+    for x = 2, c_geometry.width - 4, 4 do
+        local top_part_buffer = gdk.pixbuf_get_from_surface(c_content, x, 2, 1, 1):get_pixels()
+        local current_color = "#" .. top_part_buffer:gsub(".", function(col) return ("%02x"):format(col:byte()) end):sub(1, 6)
+        if colors[current_color] then
+            colors[current_color] = colors[current_color] + 1
+        else
+            colors[current_color] = 1
+        end
+    end
+
+    local dom_color = "#000000"
+    local dom_color_times = 0
+    for color, times in pairs(colors) do
+        if times > dom_color_times then
+            dom_color = color
+            dom_color_times = times
+        end
+    end
+
+    --naughty.notification({ title = c.name, text = dom_color })
+    return dom_color
+end
 
 function this.signal_callback(c)
     -- buttons for the titlebar
@@ -23,7 +55,7 @@ function this.signal_callback(c)
         titlebar_size = 24
     end
 
-    awful.titlebar(c, { size = titlebar_size }) : setup {
+    local titlebar_widgets = {
         {
             wibox.container.margin(nil, theme_vars.titlebar_margins, 0, 0, 0),
             wibox.container.margin(awful.titlebar.widget.iconwidget(c), 0, theme_vars.titlebar_margins / 2, theme_vars.titlebar_margins, theme_vars.titlebar_margins, nil, false),
@@ -48,6 +80,27 @@ function this.signal_callback(c)
         },
         layout = wibox.layout.align.horizontal
     }
+    -- local titlebar_bg = wibox.container.background(titlebar_widgets, "#000000")
+
+    awful.titlebar(c, { size = titlebar_size }):setup(titlebar_widgets)
+
+    --[[ gears.timer({
+        timeout = 1,
+        autostart = true,
+        call_now = false,
+        single_shot = true,
+        callback = function ()
+            titlebar_bg.bg = {
+                type = "linear",
+                from = { 0, 0 },
+                to = { 0, 32 },
+                stops = {
+                    { 0, "#1f1f1f" },
+                    { 1, get_dominant_color(c) }
+                }
+            }
+        end
+    }) ]]
 end
 
 function this.connect_signals()
