87c546108dce870308579a65101e07b8a00a3f92
ArtyIF <artyomisflash@mail.ru>
2021-10-22 14:03:58 +0300

    leftover keybinds now in separate file too

---
+++ components/keybinds/leftover_keybinds.lua
@@ -0,0 +1,91 @@
+local awful = require("awful")
+local menubar = require("menubar")
+local gears = require("gears")
+local modkeys = require("components.keybinds.modkeys")
+
+local this = {}
+
+this.group_name = "OLD KEYBINDS PLZ FIX"
+-- todo: add more keybinds?
+this.keybinds = gears.table.join(
+    awful.key({ modkeys.super,           }, "Left",   awful.tag.viewprev,
+              {description = "view previous", group = "tag"}),
+    awful.key({ modkeys.super,           }, "Right",  awful.tag.viewnext,
+              {description = "view next", group = "tag"}),
+    awful.key({ modkeys.super,           }, "Escape", awful.tag.history.restore,
+              {description = "go back", group = "tag"}),
+
+    awful.key({ modkeys.super,           }, "j",
+        function ()
+            awful.client.focus.byidx( 1)
+        end,
+        {description = "focus next by index", group = "client"}
+    ),
+    awful.key({ modkeys.super,           }, "k",
+        function ()
+            awful.client.focus.byidx(-1)
+        end,
+        {description = "focus previous by index", group = "client"}
+    ),
+    awful.key({ modkeys.super,           }, "w", function () --[[ top_wibar.main_menu.menu:show() ]] end,
+              {description = "show main menu", group = "awesome"}),
+
+    -- Layout manipulation
+    awful.key({ modkeys.super, modkeys.shift   }, "j", function () awful.client.swap.byidx(  1)    end,
+              {description = "swap with next client by index", group = "client"}),
+    awful.key({ modkeys.super, modkeys.shift   }, "k", function () awful.client.swap.byidx( -1)    end,
+              {description = "swap with previous client by index", group = "client"}),
+    awful.key({ modkeys.super, modkeys.ctrl }, "j", function () awful.screen.focus_relative( 1) end,
+              {description = "focus the next screen", group = "screen"}),
+    awful.key({ modkeys.super, modkeys.ctrl }, "k", function () awful.screen.focus_relative(-1) end,
+              {description = "focus the previous screen", group = "screen"}),
+    awful.key({ modkeys.super,           }, "u", awful.client.urgent.jumpto,
+              {description = "jump to urgent client", group = "client"}),
+    awful.key({ modkeys.super,           }, "Tab",
+        function ()
+            awful.client.focus.history.previous()
+            if client.focus then
+                client.focus:raise()
+            end
+        end,
+        {description = "go back", group = "client"}),
+
+    -- Standard program
+    awful.key({ modkeys.super,           }, "Return", function () awful.spawn(menubar.utils.terminal) end,
+              {description = "open a terminal", group = "launcher"}),
+
+    awful.key({ modkeys.super,           }, "l",     function () awful.tag.incmwfact( 0.05)          end,
+              {description = "increase master width factor", group = "layout"}),
+    awful.key({ modkeys.super,           }, "h",     function () awful.tag.incmwfact(-0.05)          end,
+              {description = "decrease master width factor", group = "layout"}),
+    awful.key({ modkeys.super, modkeys.shift   }, "h",     function () awful.tag.incnmaster( 1, nil, true) end,
+              {description = "increase the number of master clients", group = "layout"}),
+    awful.key({ modkeys.super, modkeys.shift   }, "l",     function () awful.tag.incnmaster(-1, nil, true) end,
+              {description = "decrease the number of master clients", group = "layout"}),
+    awful.key({ modkeys.super, modkeys.ctrl }, "h",     function () awful.tag.incncol( 1, nil, true)    end,
+              {description = "increase the number of columns", group = "layout"}),
+    awful.key({ modkeys.super, modkeys.ctrl }, "l",     function () awful.tag.incncol(-1, nil, true)    end,
+              {description = "decrease the number of columns", group = "layout"}),
+
+    awful.key({ modkeys.super, modkeys.ctrl }, "n",
+              function ()
+                  local c = awful.client.restore()
+                  -- Focus restored client
+                  if c then
+                    c:emit_signal(
+                        "request::activate", "key.unminimize", {raise = true}
+                    )
+                  end
+              end,
+              {description = "restore minimized", group = "client"}),
+              
+    -- Menubar
+    awful.key({ modkeys.super }, "p", function() menubar.show() end,
+              {description = "show the menubar", group = "launcher"})
+)
+
+function this.connect_keybinds(keybinds_dict)
+    return gears.table.join(keybinds_dict, this.keybinds)
+end
+
+return this
--- rc.lua
+++ rc.lua
@@ -51,6 +51,7 @@ local top_wibar = require("components.wibars.top")
 local bottom_wibar = require("components.wibars.bottom")
 
 local modkeys = require("components.keybinds.modkeys")
+local leftover_keybinds = require("components.keybinds.leftover_keybinds")
 local screenshot_spectacle_keybinds = require("components.keybinds.screenshot_spectacle")
 local sound_keybinds = require("components.keybinds.sound")
 local awesome_keybinds = require("components.keybinds.awesome")
@@ -102,87 +103,9 @@ end)
 -- }}}
 
 -- {{{ Key bindings, todo work on them
-local globalkeys = gears.table.join(
-    awful.key({ modkeys.super,           }, "Left",   awful.tag.viewprev,
-              {description = "view previous", group = "tag"}),
-    awful.key({ modkeys.super,           }, "Right",  awful.tag.viewnext,
-              {description = "view next", group = "tag"}),
-    awful.key({ modkeys.super,           }, "Escape", awful.tag.history.restore,
-              {description = "go back", group = "tag"}),
+local globalkeys = {}
 
-    awful.key({ modkeys.super,           }, "j",
-        function ()
-            awful.client.focus.byidx( 1)
-        end,
-        {description = "focus next by index", group = "client"}
-    ),
-    awful.key({ modkeys.super,           }, "k",
-        function ()
-            awful.client.focus.byidx(-1)
-        end,
-        {description = "focus previous by index", group = "client"}
-    ),
-    awful.key({ modkeys.super,           }, "w", function () top_wibar.main_menu.menu:show() end,
-              {description = "show main menu", group = "awesome"}),
-
-    -- Layout manipulation
-    awful.key({ modkeys.super, modkeys.shift   }, "j", function () awful.client.swap.byidx(  1)    end,
-              {description = "swap with next client by index", group = "client"}),
-    awful.key({ modkeys.super, modkeys.shift   }, "k", function () awful.client.swap.byidx( -1)    end,
-              {description = "swap with previous client by index", group = "client"}),
-    awful.key({ modkeys.super, modkeys.ctrl }, "j", function () awful.screen.focus_relative( 1) end,
-              {description = "focus the next screen", group = "screen"}),
-    awful.key({ modkeys.super, modkeys.ctrl }, "k", function () awful.screen.focus_relative(-1) end,
-              {description = "focus the previous screen", group = "screen"}),
-    awful.key({ modkeys.super,           }, "u", awful.client.urgent.jumpto,
-              {description = "jump to urgent client", group = "client"}),
-    awful.key({ modkeys.super,           }, "Tab",
-        function ()
-            awful.client.focus.history.previous()
-            if client.focus then
-                client.focus:raise()
-            end
-        end,
-        {description = "go back", group = "client"}),
-
-    -- Standard program
-    awful.key({ modkeys.super,           }, "Return", function () awful.spawn(terminal) end,
-              {description = "open a terminal", group = "launcher"}),
-
-    awful.key({ modkeys.super,           }, "l",     function () awful.tag.incmwfact( 0.05)          end,
-              {description = "increase master width factor", group = "layout"}),
-    awful.key({ modkeys.super,           }, "h",     function () awful.tag.incmwfact(-0.05)          end,
-              {description = "decrease master width factor", group = "layout"}),
-    awful.key({ modkeys.super, modkeys.shift   }, "h",     function () awful.tag.incnmaster( 1, nil, true) end,
-              {description = "increase the number of master clients", group = "layout"}),
-    awful.key({ modkeys.super, modkeys.shift   }, "l",     function () awful.tag.incnmaster(-1, nil, true) end,
-              {description = "decrease the number of master clients", group = "layout"}),
-    awful.key({ modkeys.super, modkeys.ctrl }, "h",     function () awful.tag.incncol( 1, nil, true)    end,
-              {description = "increase the number of columns", group = "layout"}),
-    awful.key({ modkeys.super, modkeys.ctrl }, "l",     function () awful.tag.incncol(-1, nil, true)    end,
-              {description = "decrease the number of columns", group = "layout"}),
-
-    awful.key({ modkeys.super, modkeys.ctrl }, "n",
-              function ()
-                  local c = awful.client.restore()
-                  -- Focus restored client
-                  if c then
-                    c:emit_signal(
-                        "request::activate", "key.unminimize", {raise = true}
-                    )
-                  end
-              end,
-              {description = "restore minimized", group = "client"}),
-
-    -- Prompt
-    awful.key({ modkeys.super },            "r",     function () awful.screen.focused().mypromptbox:run() end,
-              {description = "run prompt", group = "launcher"}),
-              
-    -- Menubar
-    awful.key({ modkeys.super }, "p", function() menubar.show() end,
-              {description = "show the menubar", group = "launcher"})
-)
-
+globalkeys = leftover_keybinds.connect_keybinds(globalkeys)
 globalkeys = screenshot_spectacle_keybinds.connect_keybinds(globalkeys)
 globalkeys = sound_keybinds.connect_keybinds(globalkeys)
 globalkeys = awesome_keybinds.connect_keybinds(globalkeys)
