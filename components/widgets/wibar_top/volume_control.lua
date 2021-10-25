dbf087b3e919ccf4fccf5ffb972dee4a20249595
ArtyIF <artyomisflash@mail.ru>
2021-10-25 15:05:43 +0300

    and there we go, the new volume control is here and it works!

--- components/widgets/wibar_top/volume_control.lua
+++ components/widgets/wibar_top/volume_control.lua
@@ -5,6 +5,7 @@ local awful = require("awful")
 local wibox = require("wibox")
 local theme_vars = require("beautiful").get()
 local colors = require("theme.colors")
+local button = require("components.widgets.common.button")
 
 local this = {}
 
@@ -14,6 +15,7 @@ this.get_mute_arg = "get-sink-mute 0"
 this.up_arg = "set-sink-volume 0 +5%"
 this.down_arg = "set-sink-volume 0 -5%"
 this.toggle_arg = "set-sink-mute 0 toggle"
+this.clamp_volume_arg = "set-sink-volume 0 100%"
 
 this.icons_path = colors.full_icon_theme_path .. "symbolic/status/"
 this.icon_high = "audio-volume-high-symbolic.svg"
@@ -21,25 +23,26 @@ this.icon_medium = "audio-volume-medium-symbolic.svg"
 this.icon_low = "audio-volume-low-symbolic.svg"
 this.icon_muted = "audio-volume-muted-symbolic.svg"
 
-this.margin_top = theme_vars.wibar_icon_margins
-this.margin_right = theme_vars.wibar_icon_margins
-this.margin_bottom = theme_vars.wibar_icon_margins
-this.margin_left = theme_vars.wibar_icon_margins / 2
+this.margins = theme_vars.wibar_icon_margins
 
 function this.callback(volume, muted)
     if not volume then
         return
     end
-    if muted or volume == 0 then
-        this.image_widget.image = this.icons_path .. this.icon_muted
-    elseif volume <= 33 then
-        this.image_widget.image = this.icons_path .. this.icon_low
-    elseif volume <= 67 then
-        this.image_widget.image = this.icons_path .. this.icon_medium
-    else
-        this.image_widget.image = this.icons_path .. this.icon_high
+    if volume > 100 then
+        this.execute_and_get_output(this.cmd .. this.clamp_volume_arg)
+	    volume = 100
     end
-    this.text_widget.text = " " .. volume .. "%"
+    if muted or volume == 0 then
+        this.widget:set_image(this.icons_path .. this.icon_muted)
+    elseif volume <= 33 then
+        this.widget:set_image(this.icons_path .. this.icon_low)
+    elseif volume <= 67 then
+        this.widget:set_image(this.icons_path .. this.icon_medium)
+    else
+        this.widget:set_image(this.icons_path .. this.icon_high)
+    end
+    this.widget:set_text(volume .. "%")
 end
 
 function this.execute_and_get_output(cmd)
@@ -60,54 +63,53 @@ end
 
 function this.up()
     this.execute_and_get_output(this.cmd .. this.up_arg)
-    awful.spawn.with_shell("canberra-gtk-play --id=audio-volume-change")
+    awful.spawn.spawn("canberra-gtk-play --id=audio-volume-change", false)
     return this.get()
 end
 
 function this.down()
     this.execute_and_get_output(this.cmd .. this.down_arg)
-    awful.spawn.with_shell("canberra-gtk-play --id=audio-volume-change")
+    awful.spawn.spawn("canberra-gtk-play --id=audio-volume-change", false)
     return this.get()
 end
 
 function this.toggle()
     this.execute_and_get_output(this.cmd .. this.toggle_arg)
-    awful.spawn.with_shell("canberra-gtk-play --id=audio-volume-change")
+    awful.spawn.spawn("canberra-gtk-play --id=audio-volume-change", false)
     return this.get()
 end
 
-
-this.image_widget = wibox.widget.imagebox(nil, true)
-this.text_widget = wibox.widget.textbox(nil)
-
-this.callback_timer = gears.timer({
-    timeout = 1,
-    autostart = true,
-    call_now = true,
-    callback = this.get
-})
-
 function this.create_widget()
-    this.widget = wibox.container.margin(wibox.layout.fixed.horizontal(this.image_widget, this.text_widget), this.margin_left, this.margin_right, this.margin_top, this.margin_bottom)
-
-    this.widget:buttons({
-        awful.button({ }, 1, function ()
+    this.widget = button.create_widget(
+        this.icons_path .. this.icon_muted,
+        function ()
             awful.spawn.spawn("pavucontrol")
-        end),
-        awful.button({ }, 3, function ()
-            this.toggle()
-        end),
-        awful.button({ }, 4, function ()
-            this.up()
-        end),
-        awful.button({ }, 5, function ()
-            this.down()
-        end),
+        end,
+        {
+            margins = this.margins,
+            text = "...%",
+            on_right_click = function ()
+                this.toggle()
+            end,
+            on_scroll_up = function ()
+                this.up()
+            end,
+            on_scroll_down = function ()
+                this.down()
+            end
+        }
+    )
+    
+    this.callback_timer = gears.timer({
+        timeout = 1,
+        autostart = true,
+        call_now = true,
+        callback = this.get
     })
     
     return this.widget
 end
 
-VOLUME_CONTROL = this -- todo: do something similar to other widgets
+-- VOLUME_CONTROL = this -- todo: do something similar to other widgets
 -- todo: actually, make a global registry with widgets or something
 return this
